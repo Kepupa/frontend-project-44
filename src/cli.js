0ed50b3bbcfb6738382faf7dda486779348c053e
Kepupa <2Sa1kle2@mail.ru>
2023-02-22 19:39:27 +0300

    1

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,8 @@ import readlineSync from 'readline-sync';
 function helloUser() {
   console.log('brain-games');
   console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name?');
+  const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello,${userName}!`);
-}
+  return userName;
+};
 export default helloUser;
---
+++ src/games/even.js
@@ -0,0 +1,13 @@
+import helloUser from '../src/cli.js';
+
+import readlineSync from 'readline-sync';
+const isEven = () => {
+    helloUser()
+    console.log('Answer "yes" if the number is even, otherwise answer "no".')
+
+
+}
+isEven()
+
+
+ 
