63b2c44475cf040f90dffbcebe41d02ce28de481
Kepupa <2Sa1kle2@mail.ru>
2023-03-03 13:41:44 +0300

    refactore even game

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,28 +1,4 @@
-import helloUser from '../src/cli.js';
-import getRandomNumber from '../src/randomNumber.js';
+#!/usr/bin/env node
+import playGame from "../src/games/even.js"
 
-import readlineSync from 'readline-sync';
-const isEven = (num) => num % 2 === 0; 
-
-const gameEven = () => {
-    const userName = helloUser()
-    console.log('Answer "yes" if the number is even, otherwise answer "no".')
-    for (let i = 0; i < 3; i++) { 
-        const question = getRandomNumber(0,100);
-        console.log('Question:',question)
-        const answer = readlineSync.question('Your answer: ')
-        const correctAnswer = isEven(question) ? 'yes' : 'no';
-        if (answer === correctAnswer) {
-            console.log('Correct!')
-        } else {
-            console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}.`)
-            console.log(`Let's try again, ${userName}!`);
-            break
-        }
-        console.log(`Congratulations, ${userName}!`)
-      }
-   
-    
-
-}
-gameEven()
+playGame();
--- src/games/even.js
+++ src/games/even.js
@@ -1,13 +1,19 @@
-import helloUser from '../src/cli.js';
-
-import readlineSync from 'readline-sync';
-const isEven = () => {
-    helloUser()
-    console.log('Answer "yes" if the number is even, otherwise answer "no".')
+import gameRules from '../index.js'
 
 
-}
-isEven()
+import getRandomNumber from '../randomNumber.js';
+const isEven = (num) => num % 2 === 0; 
+const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
 
+const gameEven = () => {
+    
+   const question = getRandomNumber(0,100);
+   const correctAnswer = isEven(question) ? 'yes': 'no';
+return [question,correctAnswer]  
+};
+const playGame = () =>{
+    gameRules(rule, gameEven);
+};
+export default playGame;
 
  
