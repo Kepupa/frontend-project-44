653e257a1035fe0678d9a632ebe3f3c491a4f594
Kepupa <2Sa1kle2@mail.ru>
2023-03-03 13:42:27 +0300

    refactore index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
+import { question } from 'readline-sync';
+const gameRules = (rules, game) =>{
+
+    const sdf = game();
+    const [question1, correctAnswer] = sdf;
+
+        console.log('Welcome to the Brain Games!');
+        const userName = question('May I have your name? ');
+        console.log(`Hello,${userName}!`);
+        
+    console.log(rules)
+   for (let i = 0; i < 3; i++) { 
+    const sdf = game();
+    const [question1, correctAnswer] = sdf;
+    console.log('Question:',question1)
+    const answer = question('Your answer: ')
+     //const correctAnswer ;
+    if (answer === String(correctAnswer)) {
+        console.log('Correct!')
+    } else {
+        console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}.`)
+        console.log(`Let's try again, ${userName}!`);
+        break
+    }
+     if (i === 2) console.log(`Congratulations, ${userName}!`)
+  }
 
 
 
-
-
-
-
-    
-
-
-
+}
+export default gameRules;
