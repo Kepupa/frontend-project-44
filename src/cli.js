317d5fd115b0b41d12063d5f53c81b13f1092121
Kepupa <2Sa1kle2@mail.ru>
2023-03-07 12:02:43 +0300

    fix errors #2

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,7 @@ import readlineSync from 'readline-sync';
 function helloUser() {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello,${userName}!`);
+  console.log(`Hello, ${userName}!`);
   return userName;
 }
 export default helloUser;
