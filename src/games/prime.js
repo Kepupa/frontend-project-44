437a7045e86167a6801ec68a1fd42ec00f681337
Kepupa <2Sa1kle2@mail.ru>
2023-03-09 12:41:59 +0300

    refactore isPrime function

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,16 +5,12 @@ import getRandomNumber from '../randomNumber.js';
 
 const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const isPrime = (n) => {
-  if (n > 1) {
-    for (let i = 2; i < n; i += 1) {
-      if (n % i === 0) {
-        return false;
-      }
+  for (let i = 2; i < n; i += 1) {
+    if (n % i === 0) {
+      return false;
     }
-    return true;
   }
-
-  return false;
+  return true;
 };
 
 const isPrimeGame = () => {
